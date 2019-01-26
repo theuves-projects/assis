b3c88255294ddacb707cb1f3d2281cfc495f423b
Matheus Alves <theuves@gmail.com>
2019-01-26 18:18:24 -0300

    Termina design da lista de livros

--- src/components/Home/Books/Summary.js
+++ src/components/Home/Books/Summary.js
@@ -4,7 +4,22 @@ import React from 'react'
 import './Summary.css'
 
 const Summary = () => (
-  <div></div>
+  <div className='Home_Books_Summary'>
+    <p>Brás Cubas escreve suas memórias já morto. Ele inicia a narração nos seus últimos momentos de vida, quando recebia visitas, ele viveu uma alucinação que o permitiu conhecer do primeiro ao último século.</p>
+    <p>Brás Cubas vinha de uma família rica. Era um menino arteiro, mimado e protegido pelos pais. Sem nunca ter responsabilidades e culpas, passou a infância e chegou à mocidade. Foi nela que conheceu Marcela, uma espanhola, por quem se apaixonou. Conquistou-a e viveu um romance. Brás enchia-a de caros presentes e foi assim que gastou um pouco de sua herança.</p>
+    <p>Seu pai, nessas circunstâncias, o mandou à Europa para cursar a faculdade. Ele acabou aceitando, mas pretendia partir levando Marcela, com quem tudo já estava ajustado. Porém, sua partida foi repentina e ele foi sozinho. Durante a viagem por muitos dias pensou em findar sua vida, no entanto, abandonou a idéia.</p>
+    <p>Chegado à Europa fez faculdade, formou-se e ficou vivendo lá. Voltou a chamado de seu pai, falando que voltasse ou não veria sua mãe viva. Poucos dias após a chegada de Brás sua mãe faleceu. Depois disso ele se trancou em casa e ficou a se dedicar à leitura. Passado algum tempo seu pai esteve com ele.</p>
+    <p>Tinha um plano para Brás. A carreira política e o casamento. Brás pensou por um tempo e acabou aceitando a proposta do pai. A noiva, Virgília, era bela e seu pai facilitaria a entrada na política. Mas antes de voltar para a casa do pai e seguir com os dois ideais, Brás fez uma visita à Dona Eusébia, conhecida da família ela cuidara de sua mãe antes da morte. Lá conheceu Eugênia, filha da senhora. Os dois viveram um breve romance, mas a moça era coxa, e assim como começou acabou.</p>
+    <p>Brás finalmente foi ter com a noiva e o futuro cargo na política. Mas os dois planos deram errado e Virgília acabou casada com Lobo Neves. Após tais acontecimentos o pai de Brás faleceu, a morte dele acarretou uma briga entre Brás e Sabina, sua irmã, devido à herança.</p>
+    <p>Brás voltou a viver sozinho, escrevia versos algumas vezes e era por isso que recebia a visita de Luis Dutra. Foi por intermédio desse que recebeu a notícia da chegada de Virgília e seu marido. Talvez o momento em que os dois noivaram não era adequado, mas nesse momento era o tempo e assim os dois iniciaram um romance. No início eram só valsas, mas logo arrumaram uma casa onde se encontravam.</p>
+    <p>Foi nesses tempos que Brás encontrou-se com um amigo de infância, Quincas Borba, infelizmente ele tinha se tornado um desgraçado. Mas Brás seguiu. Em certo momento de seu romance com Virgília chegou até as mãos de Lobo Neves uma carta anônima denunciando o romance dos dois. O que só desencadeou mais paixão. Brás já tinha proposto que os dois fugissem, mas a idéia foi abandonada e seguiam com o romance às escondidas.</p>
+    <p>Lobo Neves recebeu uma proposta de trabalho no ministério, na província, isso abalou o romance dos dois amantes. Mas Lobo convidou Brás para que fosse seu secretário e assim foi. Ganhada as eleições acabaram abandonando o cargo por pura superstição.</p>
+    <p>Seguiram o romance, que era acobertado por D. Plácida, que zelava da casa dos encontros. Por esse tempo Virgília engravidou e Brás sentiu o prazer da paternidade, entretanto ela perdeu a criança. Também nesse tempo Brás tinha feito as pazes com sua irmã e ela lhe buscava uma noiva.</p>
+    <p>Em uma vez, estando Virgília e Brás na casa onde se encontravam, apareceu por lá Lobo Neves sobre o pretexto de visitar D. Plácida. Brás se escondeu. Tudo acabou bem, mas daí para frente o romance dos dois amantes encerrou. Lobo acabou conquistando a presidência do ministério e eles partiram para a província.</p>
+    <p>Brás se reencontrara novamente com Quincas Borba, mas desta vez ele tinha se transformado por virtude de uma herança, os dois entraram em um estudo filosófico sobre a filosofia Humanista de Quincas. Já nesses tempos, Sabina arrumara uma noiva para Brás. Eulália Damasceno. Depois de muito refletir, Brás estava quase a firmar compromissa com a donzela, quando ela veio a falecer por febre amarela.</p>
+    <p>Daí pra frente, Brás se juntara a Quincas no praticar da filosofia Humanista, canditadou-se a político, cargo que logo perdeu, e criou um jornal que rapidamente morreu. Nos seus últimos dias viu Quincas morrer e ter o início de uma loucura, por fim morreu.</p>
+    <p>Ao fim, Brás Cubas se vangloria por não ter tido um pão compro com o suor de seu trabalho e não ter dado continuidade à humanidade.</p>
+  </div>
 )
 
 export default Summary
