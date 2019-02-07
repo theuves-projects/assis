1e75c28265d928e86671b46fef81850e900cf1d4
Matheus Alves <theuves@gmail.com>
2019-02-07 12:01:24 -0300

    Armazana preferências do usuário para livros (close #1)

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Text from './Text'
 import axios from 'axios'
+import { auth, database } from 'firebase'
 import { findBook } from '../../utils/books'
 import Loading from '../Loading'
 import './Book.css'
@@ -8,19 +9,29 @@ import './Book.css'
 class Book extends Component {
   constructor(props) {
     super(props)
-    
+
     this.changeChapter = this.changeChapter.bind(this)
     this.changeFontSize = this.changeFontSize.bind(this)
     this.changeFontFamily = this.changeFontFamily.bind(this)
     this.nextChapter = this.nextChapter.bind(this)
 
-    this.state = {
-      currentChapter: 0,
-      fontSize: 'normal',
-      fontFamily: 'serif'
+    const bookCode = this.props.match.params.code
+
+    if (this.props.data.booksConfig && this.props.data.booksConfig[bookCode]) {
+      this.state = {
+        config: this.props.data.booksConfig[bookCode]
+      }
+    } else {
+      this.state = {
+        config: {
+          currentChapter: 0,
+          fontSize: 'normal',
+          fontFamily: 'serif'
+        }
+      }
     }
 
-    const dataPath = findBook(parseInt(this.props.match.params.code)).dataPath
+    const dataPath = findBook(parseInt(bookCode)).dataPath
   
     axios.get(dataPath)
       .then((response) => {
@@ -32,24 +43,43 @@ class Book extends Component {
         alert('Houve algum erro! Não foi possível obter o livro.')
       })
   }
+  componentWillUpdate(nextProps, nextState) {
+    if (JSON.stringify(this.state) !== JSON.stringify(nextState)) {
+      const uid = auth().currentUser.uid
+      const bookCode = this.props.match.params.code
+      database().ref(`users/${uid}/booksConfig/${bookCode}`).set(nextState.config)
+    }
+  }
   changeChapter(event) {
     this.setState({
-      currentChapter: parseInt(event.target.value)
+      config: {
+        ...this.state.config,
+        currentChapter: parseInt(event.target.value)
+      }
     })
   }
   changeFontSize(event) {
     this.setState({
-      fontSize: event.target.value
+      config: {
+        ...this.state.config,
+        fontSize: event.target.value
+      }
     })
   }
   changeFontFamily(event) {
     this.setState({
-      fontFamily: event.target.value
+      config: {
+        ...this.state.config,
+        fontFamily: event.target.value
+      }
     })
   }
   nextChapter() {
     this.setState({
-      currentChapter: this.state.currentChapter + 1
+      config: {
+        ...this.state.config,
+        currentChapter: this.state.currentChapter + 1,
+      }
     })
   }
   render() {
@@ -72,7 +102,7 @@ class Book extends Component {
                 <legend className='Book-legend'>Capítulo:</legend>
                 <select
                   className='Book-select Book-selectSpecial'
-                  value={this.state.currentChapter}
+                  value={this.state.config.currentChapter}
                   onChange={this.changeChapter}
                 >
                   {this.state.data.map((chapter, index) => (
@@ -96,7 +126,7 @@ class Book extends Component {
                 <legend className='Book-legend'>Tamanho:</legend>
                 <select
                   className='Book-select'
-                  value={this.state.fontSize}
+                  value={this.state.config.fontSize}
                   onChange={this.changeFontSize}
                 >
                   <option value='small'>Fonte pequena</option>
@@ -110,7 +140,7 @@ class Book extends Component {
                 <legend className='Book-legend'>Família:</legend>
                 <select
                   className='Book-select'
-                  value={this.state.fontFamily}
+                  value={this.state.config.fontFamily}
                   onChange={this.changeFontFamily}
                 >
                   <option value='serif'>Fonte serifada</option>
@@ -121,9 +151,9 @@ class Book extends Component {
             </div>
           </div>
           <Text
-            book={this.state.data[this.state.currentChapter]}
-            fontSize={this.state.fontSize}
-            fontFamily={this.state.fontFamily}
+            book={this.state.data[this.state.config.currentChapter]}
+            fontSize={this.state.config.fontSize}
+            fontFamily={this.state.config.fontFamily}
             onReqNextChapter={this.nextChapter}
           />
         </div>
