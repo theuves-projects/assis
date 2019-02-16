efeebb4e0472bced05a239b6a99c3404183a7921
Matheus Alves <theuves@gmail.com>
2019-02-15 22:39:15 -0300

    Estiliza como "guias" os sessões do dashboard (close #21)

--- src/components/Dashboard/Content.js
+++ src/components/Dashboard/Content.js
@@ -14,47 +14,43 @@ const Content = ({
   <div className='Dashboard_Content'>
 
     {/* Header */}
-    <header className='Dashboard_Content-header'>
-      <div>
-        <Link
-          className={createClassName([
-            'Dashboard_Content-btn',
-             !option || option === 'reading' ? 'active' : null
-          ])}
-          to={!option ? `${url}/reading` : url.replace(/\w+$/, 'reading')}
-        >
-          <i className='fas fa-bookmark'></i>
-          {` `}
-          Lendo....
-        </Link>
-        <Link
-          className={createClassName([
-            'Dashboard_Content-btn',
-            option === 'read' ? 'active' : null
-          ])}
-          to={!option ? `${url}/read` : url.replace(/\w+$/, 'read')}
-        >
-          <i className='fas fa-book'></i>
-          {` `}
-          Lidos!
-        </Link>
-      </div>
+    <nav className='Dashboard_Content-tabs'>
+      <Link
+        className={createClassName([
+          'Dashboard_Content-tab',
+           !option || option === 'reading' ? 'active' : null
+        ])}
+        to={!option ? `${url}/reading` : url.replace(/\w+$/, 'reading')}
+      >
+        <i className='fas fa-bookmark'></i>
+        {` `}
+        Lendo....
+      </Link>
+      <Link
+        className={createClassName([
+          'Dashboard_Content-tab',
+          option === 'read' ? 'active' : null
+        ])}
+        to={!option ? `${url}/read` : url.replace(/\w+$/, 'read')}
+      >
+        <i className='fas fa-book'></i>
+        {` `}
+        Lidos!
+      </Link>
       {isLoggedIn ? (
-        <div>
-          <Link
-            className={createClassName([
-              'Dashboard_Content-btn-new',
-              option === 'new' ? ' active' : null
-            ])}
-            to={!option ? `${url}/new` : url.replace(/\w+$/, 'new')}
-          >
-            <i className='fas fa-plus-circle'></i>
-            {` `}
-            Novo
-          </Link>
-        </div>
+        <Link
+          className={createClassName([
+            'Dashboard_Content-tab',
+            option === 'new' ? ' active' : null
+          ])}
+          to={!option ? `${url}/new` : url.replace(/\w+$/, 'new')}
+        >
+          <i className='fas fa-plus-circle'></i>
+          {` `}
+          Novo
+        </Link>
       ) : null}
-    </header>
+    </nav>
 
     {children}
   </div>
