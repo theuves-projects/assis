03523bade673546a4c4a100ae9dccfe5340a12c6
Matheus Alves <theuves@gmail.com>
2021-06-20 21:02:04 -0400

    chore: adiciona lista de cards (livros)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
 import Header from './header'
+import Card from '../../components/card'
 import backgroundSource from '../../img/background.jpg'
+import books from '../../data/books.json'
 
 function Home() {
   return (
     <Container>
+      <Image />
       <Main>
         <Header />
+        <Content>
+          <Grid>
+            {books.map((book => (
+              <Column>
+                <Card
+                  color={book.color}
+                  title={book.title}
+                  year={book.year}
+                />
+              </Column>
+            )))}
+          </Grid>
+        </Content>
       </Main>
-      <Image />
     </Container>
   )
 }
@@ -22,6 +37,9 @@ const Container = styled.div`
 
 const Main = styled.main`
   flex: 1;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
 `
 
 const Image = styled.div`
@@ -29,6 +47,7 @@ const Image = styled.div`
   width: 500px;
   background-image: url(${backgroundSource});
   background-size: cover;
+  background-position: center;
 
   &::before {
     content: '';
@@ -36,6 +55,23 @@ const Image = styled.div`
     inset: 0;
     background-color: rgb(20 255 230 / 20%);
   }
+`
+
+const Content = styled.div`
+  padding: 20px;
+  overflow-y: auto;
+`
+
+const Grid = styled.div`
+  --grid-gap: 20px;
+  box-sizing: border-box;
+  display: grid;
+  grid-template-columns: repeat(3, calc((100% - 2 * var(--grid-gap)) / 3));
+  grid-gap: var(--grid-gap);
+`
+
+const Column = styled.div`
+
 `
 
 export default Home
