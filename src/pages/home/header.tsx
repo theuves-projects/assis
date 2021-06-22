d32e220ecf3b8183b1c889138f4c6bf8f6d31026
Matheus Alves <theuves@gmail.com>
2021-06-21 21:09:42 -0400

    chore: melhora desing dos headers

--- src/pages/home/header.tsx
+++ src/pages/home/header.tsx
@@ -1,27 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
+import quillSource from '../../img/logo.svg'
 
 function Header() {
   return (
     <HeaderStyled>
       <Container>
         <div>
-          <h1>
-            assis
-          </h1>
+          <TitleContainer>
+            <div>
+              <QuillImage src={quillSource} />
+            </div>
+            <div>
+              <Title>
+                Machado de Assis
+              </Title>
+            </div>
+          </TitleContainer>
         </div>
-        <div>
+        <SearchBoxContainer>
           <SearchBox placeholder="Buscar..." />
-        </div>
+        </SearchBoxContainer>
       </Container>
     </HeaderStyled>
   )
 }
 
 const HeaderStyled = styled.header`
-  padding: 10px 50px;
-  box-shadow: 0 0 3px 2px #ccc;
+  position: relative;
+  padding: 12px 0;
+  box-shadow: 3px 0 3px 2px rgba(0 0 0 / 30%);
   background-color: white;
+  border-bottom: dashed 1px white;
+  font-family: 'Merriweather', serif;
+  font-size: .9em;
 `
 
 const Container = styled.div`
@@ -32,11 +44,31 @@ const Container = styled.div`
   justify-content: space-between;
 `
 
+const SearchBoxContainer = styled.div`
+`
+
 const SearchBox = styled.input`
   outline: none;
   padding: 8px 16px;
-  border-radius: 16px;
-  border: solid 1px #888;
+  border: solid 1px #aaa;
+
+  &::placeholder {
+    color: #888;
+  }
+`
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const QuillImage = styled.img`
+  width: 2em;
+  margin-right: 15px;
+`
+
+const Title = styled.h1`
+  font-size: 2em;
 `
 
 export default Header
